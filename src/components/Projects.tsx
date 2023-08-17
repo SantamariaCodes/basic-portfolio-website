1a0b52112f72396256c895419b02505b11bb8531
dargen <diego.santamaria75@gmail.com>
2023-08-16 19:56:05 -0600

    feat: implementation of SocialIcons

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import SocialIcons from "./SocialIcons/SocialIcons";
 
 const Projects = React.forwardRef<HTMLDivElement>((props, ref) => (
   <div ref={ref} className="section  pb-[1000px]">
     <h2>Projects Section</h2>
-  <SocialIcons></SocialIcons>
+
   </div>
 ));
 
