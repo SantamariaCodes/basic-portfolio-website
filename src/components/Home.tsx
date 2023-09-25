e06ceb217d6b1547a0d61c35d139abb813b4af7e
Diego <diego.santamaria75@gmail.com>
2023-09-25 12:22:30 -0300

    feat(home): added padding top to home content

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -20,18 +20,20 @@ const Home: React.ForwardRefRenderFunction<HTMLDivElement, HomeProps> = (
   return (
     <div
       ref={homeRef}
-      className="section flex flex-col sm:p-10 md:p-16 items-center justify-center relative h-[70vh] sm:h-screen"
+      className="section flex flex-col sm:p-10 md:p-16 items-center justify-center relative h-[100vh] sm:h-screen"
       style={{
         backgroundImage: 'url("/resources/img/bg-home.jpeg")',
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div className=" flex flex-col items-center gap-4 p-5 sm:p-5 max-w-4xl   ">
-        <h1 className=" 420px:text-xl  sm:text-3xl md:text-4xl lg:text-5xl   text-center relative  font-extrabold">
+      <div className=" flex flex-col items-center gap-4 p-5 pt-8 sm:p-5 lg:pt-20 max-w-4xl   ">
+        <h1 className=" 420px:text-xl  sm:text-3xl md:text-4xl lg:text-5xl   text-center relative md:mb-4  font-extrabold">
           {t("title")}
         </h1>
-        <p className="text-sm md:text-xl text-center  ">{t("description")}</p>
+        <p className="text-sm   md:text-xl text-center  mb-2 md:mb-4 ">
+          {t("description")}
+        </p>
         <ProjectsButton onClick={scrollToProjects} />
       </div>
       <div className="absolute left-0 transform  hidden lg:block">
