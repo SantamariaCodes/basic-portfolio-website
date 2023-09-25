78e61ade360c40ec16e0a06179b248ec9b3ddad8
Diego <diego.santamaria75@gmail.com>
2023-09-25 11:42:54 -0300

    feat(About): redesign and enhance responsiveness of 'About Me' section
    - Update 'About Me' typography for better responsiveness across devices.
    - Enhance the skills tags to be more responsive on sm, md, and lg screens.
    - Style enhancements: bold certain text for emphasis.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,24 +9,66 @@ const About = React.forwardRef<HTMLDivElement>((props, ref) => (
       ABOUT ME
       <span className="absolute left-1/2 bottom-0 transform translate-x-[-50%] w-[70%] h-1 bg-deepBlue"></span>
     </h2>
-    <div className="text-left w-full">
-      <h3 className="text-xl font-semibold mb-4">Get to Know Me!</h3>
-      <p className="mb-8">
-        Hello! I am passionate about web development and love to work on exciting
-        projects.
-      </p>
-    </div>
-    <button className="bg-deepBlue text-white py-2 px-6 rounded mb-8">
-      CONTACT
-    </button>
-    <div className="text-left w-full">
-      <h3 className="text-xl font-semibold mb-4">My Skills</h3>
-      <div className="flex flex-wrap">
-        {['HTML', 'CSS', 'JavaScript', 'REACT', 'SASS', 'GIT','Terminal','Tailwind','AI','Boilerplate', 'SCRUM'].map((skill, index) => (
-          <span key={index} className="bg-gray-400 text-white py-1 px-4 rounded-md m-2">
-            {skill}
-          </span>
-        ))}
+    <div className="text-left w-full md:flex md:space-x-4 gap-10">
+      <div className="md:w-1/2 md:p-5">
+        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-left">
+          Get to Know Me!
+        </h3>
+        <div className="mb-8 leading-8">
+          <div className="mb-4">
+            Greetings! As a <strong>proficient front-end developer</strong>, I
+            take pride in crafting <strong>pixel-perfect UIs</strong>. With
+            extensive knowledge in modern frameworks, I ensure responsiveness
+            across all devices. My dedication is evident in every line of code,
+            ensuring optimal performance and user experience.
+          </div>
+
+          <div className="mb-4">
+            Collaborating closely with design and backend teams, I
+            <strong>bridge the gap</strong> to bring visions to life. Leveraging
+            the <strong>latest web technologies</strong>, I'm always at the
+            forefront of the digital landscape. My code is clean, maintainable,
+            and in line with the best industry practices.
+          </div>
+
+          <div>
+            Collaborating closely with design and backend teams, I
+            <strong>bridge the gap</strong> to bring visions to life. Leveraging
+            the <strong>latest web technologies</strong>
+          </div>
+        </div>
+
+        <button className="bg-deepBlue text-white py-2 px-6 lg:py-3 font-bold lg:px-8 rounded mb-8">
+          CONTACT
+        </button>
+      </div>
+      <div className="md:w-1/2 md:p-5">
+        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-left">
+          My Skills
+        </h3>
+        <div className="flex flex-wrap">
+  {[
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "REACT",
+    "SASS",
+    "GIT",
+    "Terminal",
+    "Tailwind",
+    "AI",
+    "Boilerplate",
+    "SCRUM",
+  ].map((skill, index) => (
+    <span
+      key={index}
+      className="bg-gray-300 text-sm sm:text-base md:text-base lg:text-lg text-gray-500 font-bold py-2 px-3 sm:py-2 sm:px-4 md:py-2 md:px-4 lg:py-3 lg:px-5 rounded-md m-2"
+    >
+      {skill}
+    </span>
+  ))}
+</div>
+
       </div>
     </div>
   </div>
