6df71d3e2d81e2537af15ca3de9ab63ea183d89d
dargen <diego.santamaria75@gmail.com>
2023-09-26 10:48:58 -0600

    feat(about): improved ui on about me by setting a max with

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,18 +3,18 @@ import React from "react";
 const About = React.forwardRef<HTMLDivElement>((props, ref) => (
   <div
     ref={ref}
-    className="section pb-[1000px] flex flex-col items-center md:pt-10 p-5"
+    className="section  flex flex-col items-center md:pt-10 p-5 bg-white  pb-20"
   >
-    <h2 className="mb-4 pb-4 420px:text-lg sm:text-2xl md:text-3xl lg:text-4xl text-center relative font-extrabold">
+    <h2 className="my-5 pb-2 md:pb-4  lg:mb-12 420px:text-lg sm:text-2xl md:text-3xl lg:text-4xl text-center relative font-extrabold">
       ABOUT ME
       <span className="absolute left-1/2 bottom-0 transform translate-x-[-50%] w-[70%] h-1 bg-deepBlue"></span>
     </h2>
-    <div className="text-left w-full md:flex md:space-x-4 gap-10">
+    <div className="text-left w-full md:flex md:space-x-4 gap-10  max-w-[1420px]">
       <div className="md:w-1/2 md:p-5">
         <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-left">
           Get to Know Me!
         </h3>
-        <div className="mb-8 leading-8">
+        <div className="mb-8 leading-8 xl:text-lg">
           <div className="mb-4">
             Greetings! As a <strong>proficient front-end developer</strong>, I
             take pride in crafting <strong>pixel-perfect UIs</strong>. With
@@ -47,28 +47,27 @@ const About = React.forwardRef<HTMLDivElement>((props, ref) => (
           My Skills
         </h3>
         <div className="flex flex-wrap">
-  {[
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "REACT",
-    "SASS",
-    "GIT",
-    "Terminal",
-    "Tailwind",
-    "AI",
-    "Boilerplate",
-    "SCRUM",
-  ].map((skill, index) => (
-    <span
-      key={index}
-      className="bg-gray-300 text-sm sm:text-base md:text-base lg:text-lg text-gray-500 font-bold py-2 px-3 sm:py-2 sm:px-4 md:py-2 md:px-4 lg:py-3 lg:px-5 rounded-md m-2"
-    >
-      {skill}
-    </span>
-  ))}
-</div>
-
+          {[
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "REACT",
+            "SASS",
+            "GIT",
+            "Terminal",
+            "Tailwind",
+            "AI",
+            "Boilerplate",
+            "SCRUM",
+          ].map((skill, index) => (
+            <span
+              key={index}
+              className="bg-gray-300 text-sm sm:text-base md:text-base lg:text-lg text-gray-500 font-bold py-2 px-3 sm:py-2 sm:px-4 md:py-2 md:px-4 lg:py-3 lg:px-5 rounded-md m-2"
+            >
+              {skill}
+            </span>
+          ))}
+        </div>
       </div>
     </div>
   </div>
