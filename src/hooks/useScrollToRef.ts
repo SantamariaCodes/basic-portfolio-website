d57cd4a383e0b87e9e20336eb8a695f81f779f22
dargen <diego.santamaria75@gmail.com>
2023-09-27 15:16:58 -0600

    feat: enhance scrolling behavior in useScrollToRef hook
    - Implement custom easing for smoother scrolling experience.
    - Increase duration for more gradual scrolling, especially on mobile.

--- src/hooks/useScrollToRef.ts
+++ src/hooks/useScrollToRef.ts
@@ -1,12 +1,31 @@
 import { useCallback } from "react";
 
-const useScrollToRef = (ref: React.RefObject<HTMLDivElement>, offset = 75) => {
+const useScrollToRef = (ref: React.RefObject<HTMLDivElement>, offset = 75, duration = 2200) => {
   return useCallback(() => {
     if (ref.current) {
-      const top = ref.current.getBoundingClientRect().top + window.pageYOffset - offset;
-      window.scrollTo({ top, behavior: "smooth" });
+      const start = window.scrollY;
+      const to = ref.current.getBoundingClientRect().top + start - offset;
+      let startTime: number | null = null;
+
+      const animate = (currentTime: number) => {
+        if (startTime === null) startTime = currentTime;
+        const progress = currentTime - (startTime as number);
+        const scroll = easeInOutQuad(progress, start, to - start, duration);
+        window.scrollTo(0, scroll);
+        if (progress < duration) window.requestAnimationFrame(animate);
+      };
+      
+      window.requestAnimationFrame(animate);
     }
-  }, [ref, offset]);
+  }, [ref, offset, duration]);
+};
+
+
+const easeInOutQuad = (t: number, b: number, c: number, d: number) => {
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t + b;
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
 };
 
 export default useScrollToRef;
