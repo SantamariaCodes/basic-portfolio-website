25aa4b9c234dd38ad1242edce191b758deee36c9
dargen <diego.santamaria75@gmail.com>
2023-09-28 08:24:44 -0600

    feat(footer): implemented basic footer both on mobile and desktop

---
+++ src/components/Footer.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import LinkedInIcon from "./SocialIcons/LinkedInIcon";
+import TwitterIcon from "./SocialIcons/TwitterIcon";
+import GitHubIcon from "./SocialIcons/GitHubIcon";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-black text-white h-[400px] py-20">
+      <div className="container mx-auto max-w-7xl flex flex-col-reverse sm:flex-row justify-between items-center">
+        <div>
+          <h2 className="text-2xl font-bold pb-6 sm:mb-0  ">
+            DIEGO SANTAMARIA
+          </h2>
+          <p className=" font-semibold leading-7">
+            A Frontend focused Web Developer building the Frontend of Websites a
+          </p>
+          <p className=" font-semibold leading-7">
+            A Frontend focused Web Developer building the Frontend of Websites \
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center mb-8 sm:mb-0">
+          <h3 className="text-2xl font-semibold mb-6">SOCIAL</h3>
+          <div className="flex space-x-6">
+            <a
+              href="https://linkedin.com/in/your-linkedin-id"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300 transition duration-200"
+            >
+              <LinkedInIcon className="w-8 h-8" />
+            </a>
+            <a
+              href="https://twitter.com/your-twitter-handle"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300 transition duration-200"
+            >
+              <TwitterIcon className="w-8 h-8" />
+            </a>
+            <a
+              href="https://github.com/your-github-username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300 transition duration-200"
+            >
+              <GitHubIcon className="w-8 h-8" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -4,6 +4,7 @@ import Home from "../components/Home";
 import About from "../components/About";
 import Projects from "../components/Projects/Projects";
 import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer";
 
 function Portfolio() {
   const homeRef = useRef(null);
@@ -23,6 +24,7 @@ function Portfolio() {
       <About aboutRef={aboutRef} contactRef={contactRef} />
       <Projects ref={projectsRef} />
       <Contact ref={contactRef} />
+      <Footer/>
     </div>
   );
 }
