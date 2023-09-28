20eeaad784ef943899043dcf6ee9cf9645a7d108
dargen <diego.santamaria75@gmail.com>
2023-09-28 09:00:36 -0600

    feat(footer): enhanced mobile responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,17 +6,15 @@ import GitHubIcon from "./SocialIcons/GitHubIcon";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-black text-white h-[400px] py-20">
-      <div className="container mx-auto max-w-7xl flex flex-col-reverse sm:flex-row justify-between items-center">
+      <div className="container p-5 md:p-10 mx-auto max-w-7xl flex flex-col-reverse sm:flex-row justify-between items-center">
         <div>
-          <h2 className="text-2xl font-bold pb-6 sm:mb-0  ">
+          <h2 className="text-2xl md:text-left text-center font-bold pb-6 sm:mb-0  ">
             DIEGO SANTAMARIA
           </h2>
-          <p className=" font-semibold leading-7">
+          <p className=" text-center md:text-left  font-semibold leading-7">
             A Frontend focused Web Developer building the Frontend of Websites a
           </p>
-          <p className=" font-semibold leading-7">
-            A Frontend focused Web Developer building the Frontend of Websites \
-          </p>
+      
         </div>
         <div className="flex flex-col items-center text-center mb-8 sm:mb-0">
           <h3 className="text-2xl font-semibold mb-6">SOCIAL</h3>
