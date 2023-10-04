81f4291fcc88805a80992e1d0e6e23416da541d9
dargen <diego.santamaria75@gmail.com>
2023-10-04 13:48:07 -0600

    fix(footer): enable navigation through social icons with anchor tags

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,23 +15,32 @@ const Footer: React.FC = () => {
           </h2>
 
           <div
-            className="text-center md:text-left font-semibold leading-6 md:hidden" 
+            className="text-center md:text-left font-semibold leading-6 md:hidden"
             dangerouslySetInnerHTML={{ __html: t("footerDescription") }}
           />
 
           <div
-            className="hidden md:block text-center md:text-left font-semibold leading-4" 
+            className="hidden md:block text-center md:text-left font-semibold leading-4"
             dangerouslySetInnerHTML={{ __html: t("footerDescriptionDesktop") }}
           />
         </div>
         <div className="flex flex-col items-center text-center mb-8 sm:mb-0">
           <h3 className="text-2xl font-semibold mb-6"> {t("socialHeader")}</h3>
           <div className="flex space-x-6">
-            <LinkedInIcon className="w-8 h-8 hover:text-gray-300 " />
+            <LinkedInIcon
+              className="w-8 h-8 hover:text-gray-300 "
+              href="https://www.linkedin.com/in/diego-santamaria-miguel"
+            />
 
-            <TwitterIcon className="w-8 h-8 hover:text-gray-300 " />
+            <TwitterIcon
+              className="w-8 h-8 hover:text-gray-300"
+              href="https://twitter.com/DiegoSM47"
+            />
 
-            <GitHubIcon className="w-8 h-8 hover:text-gray-200" />
+            <GitHubIcon
+              className="w-8 h-8 hover:text-gray-200"
+              href="https://github.com/CodeSantamaria"
+            />
           </div>
         </div>
       </div>
