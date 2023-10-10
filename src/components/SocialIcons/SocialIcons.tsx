e22a872a1a3b500d2ed6076c1fbeba0f61f9e2d0
dargen <diego.santamaria75@gmail.com>
2023-10-10 14:37:54 -0600

    feat: implement embeddedLink prop for social icons

--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -2,27 +2,32 @@ import React from "react";
 import TwitterIcon from "./TwitterIcon";
 import LinkedInIcon from "./LinkedInIcon";
 import GitHubIcon from "./GitHubIcon";
-
 const SocialIcons: React.FC = () => (
   <div className="flex flex-col bg-white w-16 p-2  items-center justify-center  shadow-2xl ">
-    <div
+    <a
+      href="https://twitter.com/DiegoSM47"
+      target="_blank"
       rel="noopener noreferrer"
       className="p-4 hover:bg-deepBlue rounded transition "
     >
       <TwitterIcon className="h-6 w-6 text-black" />
-    </div>
-    <div
+    </a>
+    <a
+      href="https://www.linkedin.com/in/diego-santamaria-miguel"
+      target="_blank"
       rel="noopener noreferrer"
       className="p-4 hover:bg-deepBlue rounded transition"
     >
       <LinkedInIcon className="h-6 w-6 text-black" />
-    </div>
-    <div
+    </a>
+    <a
+      href="https://github.com/CodeSantamaria"
+      target="_blank"
       rel="noopener noreferrer"
       className="p-4 hover:bg-deepBlue rounded transition"
     >
       <GitHubIcon className="h-6 w-6 text-black" />
-    </div>
+    </a>
   </div>
 );
 
