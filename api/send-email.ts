44ca8c731809c4b2d8ffbc11574ee8f00e046412
dargen <diego.santamaria75@gmail.com>
2023-10-10 11:07:21 -0600

    feat:integrate Resend API for email functionality

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,33 +1,26 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import axios from 'axios';
+// /api/send-email.js
+import { Resend } from 'resend';
 
-const RESEND_API_KEY = process.env.RESEND_API_KEY;
+const API_KEY = 're_MQzeWfFs_6UP761ERjUK7RAGauyX8cQCx';
+const resend = new Resend(API_KEY);
 
-const sendEmail = async (req: VercelRequest, res: VercelResponse) => {
-  if (req.method !== 'POST') {
-    return res.status(405).end();
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
+
+    try {
+      const response = await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: 'diego.santamaria750@gmail.com',
+        subject: `New Message from ${name} - ${email}`,
+        html: `<p>${message}</p>`
+      });
+
+      return res.status(200).json({ success: true, data: response });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message });
+    }
+  } else {
+    return res.status(405).json({ error: 'Method not allowed' });
   }
-
-  const { name, email, message } = req.body;
-
-  try {
-    const response = await axios.post('https://api.resend.io/emails/send', {
-      from: 'onboarding@resend.dev',
-      to: email,
-      subject: `Contact form submission from ${name}`,
-      html: `<p>${message}</p>`
-    }, {
-      headers: {
-        'Authorization': `Bearer ${RESEND_API_KEY}`
-      }
-    });
-
-    res.status(200).json({ success: true, data: response.data });
-} catch (error) {   // <-- This is the start of the catch block
-  console.error('Server-Side Detailed Error:', error); // Add this line
-
-  res.status(500).json({ success: false, message: 'Error sending email.' });
 }
-
-};
-export default sendEmail;
--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -4,34 +4,35 @@ import React from "react";
 import { Formik, Form, Field } from "formik";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
+import { useState } from "react";
 interface ContactFormProps {
-  onSubmit: (values: { name: string; email: string; message: string }) => void;
+  onSubmit?: (values: { name: string; email: string; message: string }) => void;
 }
-
 const ContactForm: React.FC<ContactFormProps> = ({ onSubmit }) => {
   const { t } = useTranslation("contact");
+  const [feedback, setFeedback] = useState<string | null>(null);
 
   return (
     <Formik
-      initialValues={{ name: "", email: "", message: "" }}
-      onSubmit={(values, actions) => {
-        console.log("Form onSubmit triggered!", values);
+    initialValues={{ name: "", email: "", message: "" }}
+    onSubmit={(values, actions) => {
+      console.log("Form onSubmit triggered!", values);
 
-        axios
-          .post("/api/send-email", values) // Call the serverless function
-          .then((response) => {
-            console.log(response.data);
-            actions.setSubmitting(false);
-            // Maybe show a success message
-          })
-          .catch((error) => {
-            actions.setSubmitting(false);
-        
-
-            // Maybe show an error message
-          });
-      }}
-    >
+      axios
+        .post("/api/send-email", values) // Call the serverless function
+        .then((response) => {
+          console.log(response.data);
+          actions.setSubmitting(false);
+          
+          setFeedback("Your message was successfully sent!"); // Success feedback
+        })
+        .catch((error) => {
+          actions.setSubmitting(false);
+          
+          setFeedback("There was an error sending your message. Please try again later."); // Error feedback
+        });
+    }}
+  >
       {({ isSubmitting }) => (
         <Form>
           <label className="block mb-6 ">
@@ -69,6 +70,12 @@ const ContactForm: React.FC<ContactFormProps> = ({ onSubmit }) => {
               placeholder={t("messagePlaceholder")}
             />
           </label>
+          {feedback && (
+            <div className="mt-4 text-center text-red-600"> {/* adjust styles as needed */}
+              {feedback}
+            </div>
+          )}
+
           <button
             type="submit"
             className="bg-deepBlue hover:bg-lessDeepBlue text-white py-2 px-6 lg:py-3 font-bold lg:px-8 rounded "
@@ -76,7 +83,9 @@ const ContactForm: React.FC<ContactFormProps> = ({ onSubmit }) => {
           >
             {t("submitButton")}
           </button>
+          
         </Form>
+        
       )}
     </Formik>
   );
